eeffc57f9ff94f34f07560b356ea49fcfc826da1
michael haruo nakashima <mhnakashima@gmail.com>
2021-02-14 21:29:51 -0300

    styling header

--- src/components/headerBar/headerBar.js
+++ src/components/headerBar/headerBar.js
@@ -1,18 +1,24 @@
 import React from "react";
-import Pic from "./pic/pic";
-import Info from "./info/info";
-import ContactMe from "./contactMe/contactMe";
-
-import './headerBar.scss';
-
+import "./headerBar.scss";
 class HeaderBar extends React.Component {
   render() {
     return (
-    <div className="headerBar">
-      <Pic className='pic' />
-      <Info className='info' />
-      <ContactMe className='contactMe' />
-    </div>
+      <div className="headerBar">
+        <span className="headerBar__status">
+          Available for freelas <span className="status available"></span>
+          <ul className="languages">
+            <li>
+              <button className="languages__button">PT</button>
+            </li>
+            <li>
+              <button className="languages__button">ES</button>
+            </li>
+            <li>
+              <button className="languages__button">EN</button>
+            </li>
+          </ul>
+        </span>
+      </div>
     );
   }
 }
